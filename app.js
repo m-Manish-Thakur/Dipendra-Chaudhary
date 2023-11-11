cb891440735b0189eb55de1660e56e599a3a5796
Manish Thakur <manishthakur231690@gmail.com>
2023-11-11 08:01:52 +0530

    Added

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
-// Header Animation
+document.addEventListener("DOMContentLoaded", function () {
+  const projectsList = document.getElementById("projects-list");
+  const filterButtons = document.querySelectorAll(".filterBtn");
+  let data;
+  // Function to handle filter button click
+  function handleFilterClick(button) {
+    filterButtons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const filter = button.getAttribute("data-filter");
+    const filteredProjects = data.filter((project) => {
+      if (filter === "all") {
+        return true; // Show all projects
+      } else {
+        return project.category === filter;
+      }
+    });
+
+    displayProjects(filteredProjects);
+  }
+
+  // Load JSON data (assuming you have a JSON file with project data)
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      data = jsonData;
+      displayProjects(data);
+
+      filterButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          handleFilterClick(button);
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error loading JSON data:", error);
+    });
+
+  function displayProjects(projects) {
+    projectsList.innerHTML = "";
+    projects.forEach((project) => {
+      const projectItem = document.createElement("div");
+      projectItem.innerHTML = `
+      <div class="item">
+                  <iframe src="${project.video}"></iframe>
+                  <h3>${project.title}</h3>
+                 <p>${project.category}</p>
+                </div>
+          `;
+      projectsList.appendChild(projectItem);
+    });
+  }
+});
 
 window.onscroll = function () {
   scrollFunction();
@@ -52,3 +104,11 @@ function deletingEffect() {
 }
 
 typingEffect();
+
+// -------------------------   Contact Form   -------------------------------------
+
+window.onbeforeunload = () => {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("desc").value = "";
+};
